687674b42e23e6b771cf18f3aebd362ad1ee12a3
Surbhi Rai <surbhi.rai1998@gmail.com>
2020-04-12 10:21:58 +0530

    Basic structure and models added

---
+++ FairFareWebsite/Controllers/PricePredictionController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using FairFareML.Model;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FairFareWebsite.Controllers
+{
+    public class PricePredictionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // GET: /<controller>/
+        public IActionResult Predict_price(ModelInput input)
+        {
+            MLContext mlContext = new MLContext();
+            ITransformer mlModel = mlContext.Model.Load(@"..\FairFareML.Model\MLModel.zip", out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            ModelOutput result = predEngine.Predict(input);
+
+            ViewBag.Price = result.Score;
+            return View(input);
+        }
+    }
+}
+  
